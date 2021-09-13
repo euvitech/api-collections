7ddafa44021bd0562427fb4cef0cf098a5064b4a
Vinicius Pereira <vguard.2003@gmail.com>
2021-09-13 16:41:48 -0300

    created TestOperacoes and testing Arrays type of operation

---
+++ src/main/kotlin/vguard/study-collections/TestOperacoes.kt
@@ -0,0 +1,28 @@
+package vguard.`study-collections`
+
+fun main() {
+    val salarys = doubleArrayOf(1200.0, 3000.0, 5000.0)
+
+    println("===========================")
+
+        println("""
+            Salário minimo: ${salarys.minOrNull()}
+            Salário médio:  ${salarys.average()}
+            Salário maximo: ${salarys.maxOrNull()}
+        """.trimIndent())
+
+    println("===========================")
+
+    val maxSalary = salarys.filter { it > 4000.0 }
+
+    maxSalary.forEach(){
+        println(it)
+    }
+
+    println("===========================")
+
+    println(salarys.count(){it in 2452.0..5000.0})
+
+    println("===========================")
+
+}
